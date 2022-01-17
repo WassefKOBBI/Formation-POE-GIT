184c3233301f906f46f1d2091b3a82f3a801f3ec
kobbi <wassef.kobbi@gmail.com>
2022-01-17 14:15:50 +0100

    refactor des noms des fonctions

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function somme(nombre1, nombre2) {
+
+    return nombre1 + nombre2;
+    
+    }
+    
+    
+    
+    function division(nombre1, nombre2) {
+    
+    return nombre1 / nombre2;
+    
+    }
